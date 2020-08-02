8bce71f44832e7a4a58f4baf47634f65dee987bb
jaysinco <jaysinco@163.com>
2020-08-02 22:38:30 +0800

    format arp packet

--- src/net.cpp
+++ src/net.cpp
@@ -1,28 +1,39 @@
 #include "net.h"
 
+std::ostream &operator<<(std::ostream &out, const in_addr &addr)
+{
+    u_long ip4 = addr.s_addr;
+    auto c = reinterpret_cast<const u_char*>(&ip4);
+    out << int(c[0]);
+    for (int i = 1; i < 4; ++i) {
+        out << "." << int(c[i]);
+    }
+    return out;
+}
+
+std::ostream &operator<<(std::ostream &out, const in6_addr &addr)
+{
+    const u_short *h = addr.u.Word;
+    out << std::hex << ntohs(h[0]);
+    for (int i = 1; i < 8; ++i) {
+        out << ":" << ntohs(h[i]);
+    }
+    return out << std::dec;
+}
+
 std::ostream &operator<<(std::ostream &out, const sockaddr *addr)
 {
     if (addr->sa_family == AF_INET) {
-        u_long ipv4 = reinterpret_cast<const sockaddr_in*>(addr)->sin_addr.s_addr;
-        const u_char *c = reinterpret_cast<u_char*>(&ipv4);
-        out << int(c[0]);
-        for (int i = 1; i < 4; ++i) {
-            out << "." << int(c[i]);
-        }
+        out << reinterpret_cast<const sockaddr_in*>(addr)->sin_addr;
     }
     else if (addr->sa_family == AF_INET6) {
-        const u_short *h = reinterpret_cast<const sockaddr_in6*>(addr)->sin6_addr.u.Word;
-        out << std::hex << ntohs(h[0]);
-        for (int i = 1; i < 8; ++i) {
-            out << ":" << ntohs(h[i]);
-        }
-        out << std::dec;
+        out << reinterpret_cast<const sockaddr_in6*>(addr)->sin6_addr;
     }
     else if (addr->sa_family == AF_UNSPEC) {
         out << "(unspecified)";
     }
     else {
-        out << "(unknow family: " << addr->sa_family << ")";
+        LOG(ERROR) << "unknow ip address family: " << addr->sa_family;
     }
     return out;
 }
@@ -42,14 +53,74 @@ std::ostream &operator<<(std::ostream &out, const pcap_if_t *dev)
     return out;
 }
 
-std::ostream &print_packet(std::ostream &out, const pcap_pkthdr *header, const u_char *data)
+std::ostream &operator<<(std::ostream &out, const ip4_addr &addr)
+{
+    in_addr cvt;
+    cvt.S_un.S_un_b.s_b1 = addr.b1;
+    cvt.S_un.S_un_b.s_b2 = addr.b2;
+    cvt.S_un.S_un_b.s_b3 = addr.b3;
+    cvt.S_un.S_un_b.s_b4 = addr.b4;
+    return out << cvt;
+}
+
+std::ostream &operator<<(std::ostream &out, const eth_addr &addr)
+{
+    auto c = reinterpret_cast<const u_char*>(&addr);
+    out << std::hex << int(c[0]);
+    for (int i = 1; i < 6; ++i) {
+        out << "-" << int(c[i]);
+    }
+    return out << std::dec;
+}
+
+std::ostream &operator<<(std::ostream &out, const eth_ip4_arp *arp_data)
+{
+    if (ntohs(arp_data->hw_type) != 1 || ntohs(arp_data->proto) != 0x0800 ||
+        arp_data->hw_len != 6 || arp_data->proto_len != 4)
+    {
+        out << "(not typical ethernet ipv4 arp)";
+        return out;
+    }
+    switch (ntohs(arp_data->op)) {
+    case 1: // arp request
+        out << "[ARP] " << arp_data->sia << ": who is " << arp_data->dia << "?";
+        break;
+    case 2: // arp reply
+        out << "[ARP] " << arp_data->sia << ": i am at " << arp_data->sea << ".";
+        break;
+    case 3: // rarp request
+        out << "[RARP]";
+        break;
+    case 4: // rarp reply
+        out << "[RARP]";
+        break;
+    }
+    return out;
+}
+
+std::ostream &print_packet(std::ostream &out, const pcap_pkthdr *header, const u_char *pkt_data)
 {
     time_t local_tv_sec = header->ts.tv_sec;
     tm ltime;
     localtime_s(&ltime, &local_tv_sec);
     char timestr[16];
     strftime(timestr, sizeof(timestr), "%H:%M:%S", &ltime);
-    out << timestr << "." << std::setw(6) << std::left << header->ts.tv_usec;
-    out << "  len: " << header->len;
+    out << timestr << "." << std::setw(6) << std::left << header->ts.tv_usec << " ";
+    auto eh = reinterpret_cast<const ethernet_header*>(pkt_data);
+    switch (ntohs(eh->eth_type))
+    {
+    case 0x0800: // ip
+        out << "[IP]";
+        break;
+    case 0x0806: // arp
+    case 0x8035: // rarp
+    {
+        auto ah = reinterpret_cast<const eth_ip4_arp*>(pkt_data + sizeof(ethernet_header));
+        out << ah;
+        break;
+    }
+    default:
+        LOG(ERROR) << "unknow ethernet type: " << eh->eth_type;
+    }
     return out;
 }
--- src/net.h
+++ src/net.h
@@ -5,6 +5,57 @@
 #include <glog/logging.h>
 #include "pcap.h"
 
+struct eth_addr {
+    u_char b1, b2, b3, b4, b5, b6;
+};
+
+struct ip4_addr {
+    u_char b1, b2, b3, b4;
+};
+
+struct ethernet_header {
+    eth_addr dea;      // Destination address
+    eth_addr sea;      // Source address
+    u_short  eth_type; // Ethernet type
+};
+
+struct eth_ip4_arp {
+    u_short  hw_type;   // Hardware type
+    u_short  proto;     // Protocol
+    u_char   hw_len;    // Hardware address length
+    u_char   proto_len; // Protocol address length
+    u_short  op;        // Operation code
+    eth_addr sea;       // Source ethernet address
+    ip4_addr sia;       // Source ip address
+    eth_addr dea;       // Destination ethernet address
+    ip4_addr dia;       // Destination ip address
+};
+
+struct ip4_header {
+    u_char   ver_ihl;        // Version (4 bits) + Internet header length (4 bits)
+    u_char   tos;            // Type of service
+    u_short  tlen;           // Total length
+    u_short  identification; // Identification
+    u_short  flags_fo;       // Flags (3 bits) + Fragment offset (13 bits)
+    u_char   ttl;            // Time to live
+    u_char   proto;          // Protocol
+    u_short  crc;            // Header checksum
+    ip4_addr sia;            // Source address
+    ip4_addr dia;            // Destination address
+};
+
+struct udp_header {
+    u_short sport; // Source port
+    u_short dport; // Destination port
+    u_short len;   // Datagram length
+    u_short crc;   // Checksum
+};
+
+std::ostream &operator<<(std::ostream &out, const in_addr &addr);
+std::ostream &operator<<(std::ostream &out, const in6_addr &addr);
 std::ostream &operator<<(std::ostream &out, const sockaddr *addr);
 std::ostream &operator<<(std::ostream &out, const pcap_if_t *dev);
-std::ostream &print_packet(std::ostream &out, const pcap_pkthdr *header, const u_char *data);
+std::ostream &operator<<(std::ostream &out, const ip4_addr &addr);
+std::ostream &operator<<(std::ostream &out, const eth_addr &addr);
+std::ostream &operator<<(std::ostream &out, const eth_ip4_arp *arp_data);
+std::ostream &print_packet(std::ostream &out, const pcap_pkthdr *header, const u_char *pkt_data);
