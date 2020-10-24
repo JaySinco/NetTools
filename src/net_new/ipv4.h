8ac45b05460da687e5af3d4a231986e35b5432da
jaysinco <jaysinco@163.com>
2020-10-25 00:20:13 +0800

    add forgot override

--- src/net_new/ipv4.h
+++ src/net_new/ipv4.h
@@ -5,7 +5,7 @@ class NtP_IPv4 : public NtProtocol
 public:
     NtP_IPv4(const byte *data, byte *&after);
     virtual ~NtP_IPv4();
-    virtual void to_bytes(std::vector<byte> &pkt_data) const;
+    virtual void to_bytes(std::vector<byte> &pkt_data) const override;
     virtual std::string type() const override;
     virtual std::string str() const override;
 };
