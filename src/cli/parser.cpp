86e495017fc2f77edf0b2540e6cef1bbea9f6ecf
jaysinco <jaysinco@163.com>
2020-12-07 19:22:17 +0800

    add fusion adapt struct

--- src/cli/parser.cpp
+++ src/cli/parser.cpp
@@ -1,8 +1,18 @@
 #include "prec.h"
-#include <boost/fusion/include/std_tuple.hpp>
+#include <boost/config/warning_disable.hpp>
 #include <boost/spirit/home/x3.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/std_tuple.hpp>
+#include <boost/fusion/include/io.hpp>
 
 namespace x3 = boost::spirit::x3;
+
+struct clothes
+{
+    std::string name;
+    int size;
+};
+BOOST_FUSION_ADAPT_STRUCT(clothes, name, size);
 
 struct tens_type : x3::symbols<unsigned>
 {
@@ -12,8 +22,9 @@ struct tens_type : x3::symbols<unsigned>
 auto set_zero = [&](auto &ctx) { _val(ctx) = 0; };
 auto add = [&](auto &ctx) { _val(ctx) += _attr(ctx); };
 
-const x3::rule<class parser, double> parser = "parser";
-const auto parser_def = x3::eps[set_zero] >> tens[add] % ',';
+const x3::rule<class parser, clothes> parser = "parser";
+const auto quoted_string = x3::lexeme['"' >> +(x3::char_ - '"') >> '"'];
+const auto parser_def = x3::lit("clothes") >> '{' >> quoted_string >> ',' >> tens >> '}';
 BOOST_SPIRIT_DEFINE(parser);
 
 int main(int argc, char *argv[])
@@ -23,9 +34,9 @@ int main(int argc, char *argv[])
     FLAGS_logtostderr = 1;
     FLAGS_minloglevel = 0;
 
-    std::string s = "X, XX, L";
-    double sum = 0;
+    std::string s = R"(clothes{ "test", XL })";
+    clothes ast;
     auto it = s.begin();
-    x3::phrase_parse(it, s.end(), parser, x3::space, sum);
-    LOG(INFO) << sum;
+    x3::phrase_parse(it, s.end(), parser, x3::space, ast);
+    LOG(INFO) << fmt::to_string(std::make_tuple(ast.name, ast.size));
 }
