0768c089203e1ae1687e7c1a8bb013f33ea94321
jaysinco <jaysinco@163.com>
2020-12-07 18:40:34 +0800

    fix completion

--- build-completion.bash
+++ build-completion.bash
@@ -3,7 +3,7 @@
 _build_completions()
 {
     if [ -d "dest" ]; then
-        COMPREPLY=($(compgen -W "$(ls dest/*.vcxproj | grep -v [[:upper:]] | awk -F [/.] '{print $2}')" -- "${COMP_WORDS[-1]}"))
+        COMPREPLY=($(compgen -W "$(find dest -maxdepth 1 -name *.vcxproj | grep -v [[:upper:]] | awk -F [/.] '{print $2}')" -- "${COMP_WORDS[-1]}"))
     fi
 }
 
