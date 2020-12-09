93805ee524dd593a7daaebe518d2d03c7464abd2
jaysinco <jaysinco@163.com>
2020-12-09 23:07:44 +0800

    parser compile ok

--- src/cli/test.cpp
+++ src/cli/test.cpp
@@ -1,11 +1,16 @@
 #include "prec.h"
+#include "core/validator.h"
 
 int main(int argc, char *argv[])
 {
+    NT_TRY
     google::InitGoogleLogging(argv[0]);
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     FLAGS_logtostderr = 1;
     FLAGS_minloglevel = 0;
 
-    LOG(INFO) << "test";
+    validator::from_str(R"(
+        ipv4.id.0="123" & udp.port = 12
+    )");
+    NT_CATCH
 }
--- src/core/validator.cpp
+++ src/core/validator.cpp
@@ -3,7 +3,6 @@
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
-#include <boost/fusion/include/at_c.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/std_tuple.hpp>
 #include <boost/fusion/include/std_pair.hpp>
@@ -122,51 +121,35 @@ private:
 //  parser definition
 ///////////////////////////////////////////////////////////////////////////
 
-namespace parser
+namespace ast
 {
-struct select_attr_value
-{
-    std::vector<std::string> v;
-};
-
-struct conditon_value
-{
-    select_attr_value v;
-    boost::optional<std::string> s;
-};
-
 struct expression_value;
 
-struct group_value
+using select_attr_value = std::vector<std::string>;
+using conditon_value = std::pair<select_attr_value, boost::optional<std::string>>;
+using group_value = x3::forward_ast<expression_value>;
+using factor_value = boost::variant<conditon_value, group_value>;
+using term_value = std::vector<factor_value>;
+
+struct expression_value : std::vector<term_value>
 {
-    x3::forward_ast<expression_value> v;
 };
 
-struct factor_value
-{
-    boost::variant<conditon_value, group_value> v;
-};
+}  // namespace ast
 
-struct term_value
+namespace parser
 {
-    std::vector<factor_value> v;
-};
-
-struct expression_value
-{
-    std::vector<term_value> v;
-};
-
-x3::rule<class select_attr_class, select_attr_value> select_attr = "select_attr";
-x3::rule<class conditon_class, conditon_value> conditon = "conditon";
-x3::rule<class group_class, group_value> group = "group";
-x3::rule<class factor_class, factor_value> factor = "factor";
-x3::rule<class term_class, term_value> term = "term";
-x3::rule<class expression_class, expression_value> expression = "expression";
+const x3::rule<class value_class, std::string> value = "value";
+const x3::rule<class select_attr_class, ast::select_attr_value> select_attr = "select_attr";
+const x3::rule<class conditon_class, ast::conditon_value> conditon = "conditon";
+const x3::rule<class group_class, ast::group_value> group = "group";
+const x3::rule<class factor_class, ast::factor_value> factor = "factor";
+const x3::rule<class term_class, ast::term_value> term = "term";
+const x3::rule<class expression_class, ast::expression_value> expression = "expression";
 
 const auto number = +x3::char_("0-9");
 const auto quoted_string = x3::lexeme['"' >> +(x3::char_ - '"') >> '"'];
-const auto value = number | quoted_string;
+const auto value_def = number | quoted_string;
 const auto select_attr_def = +x3::char_("0-9a-zA-Z") % '.';
 const auto conditon_def = select_attr >> -('=' >> value);
 const auto group_def = '(' >> expression >> ')';
@@ -174,16 +157,9 @@ const auto factor_def = conditon | group;
 const auto term_def = factor % '&';
 const auto expression_def = term % '|';
 
-BOOST_SPIRIT_DEFINE(select_attr, conditon, group, factor, term, expression);
+BOOST_SPIRIT_DEFINE(value, select_attr, conditon, group, factor, term, expression);
 
 }  // namespace parser
-
-BOOST_FUSION_ADAPT_STRUCT(parser::select_attr_value, v);
-BOOST_FUSION_ADAPT_STRUCT(parser::conditon_value, v, s);
-BOOST_FUSION_ADAPT_STRUCT(parser::group_value, v);
-BOOST_FUSION_ADAPT_STRUCT(parser::factor_value, v);
-BOOST_FUSION_ADAPT_STRUCT(parser::term_value, v);
-BOOST_FUSION_ADAPT_STRUCT(parser::expression_value, v);
 
 bool validator::test(const packet &pac) const
 {
@@ -199,8 +175,11 @@ bool validator::test(const packet &pac) const
 p_validator validator::from_str(const std::string &code)
 {
     p_validator pv;
-    parser::expression_value ast;
+    ast::expression_value ast;
     auto it = code.begin();
-    x3::phrase_parse(it, code.end(), parser::expression, x3::space, ast);
+    bool ok = x3::phrase_parse(it, code.end(), parser::expression, x3::space, ast);
+    if (!ok || it != code.end()) {
+        throw std::runtime_error(fmt::format("failed to parse: unexpect token near '{}'", *it));
+    }
     return pv;
 }
