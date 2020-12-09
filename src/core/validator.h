afe6c9f709bb7436388539272a5947069cd8f5b9
jaysinco <jaysinco@163.com>
2020-12-10 00:15:31 +0800

    remove validator to_json method

--- src/core/validator.h
+++ src/core/validator.h
@@ -6,8 +6,6 @@ class validator
 public:
     virtual bool test(const json &j) const = 0;
 
-    // virtual json to_json() const = 0;
-
     bool test(const packet &pac) const;
 
     static std::shared_ptr<validator> from_str(const std::string &code);
