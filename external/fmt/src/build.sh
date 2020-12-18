ebedb4cb6e4daa59c180ef37118b07b87f301043
jaysinco <jaysinco@163.com>
2020-12-18 21:38:24 +0800

    fix fmt build script

--- external/fmt/src/build.sh
+++ external/fmt/src/build.sh
@@ -7,7 +7,6 @@ mkdir -p build/ \
 cd build \
 && \
 ${CMAKE_CMD} "${CMAKE_GENERATOR}" ../fmt-7.1.3 \
-    -DMSVC_RUNTIME="static" \
     -DFMT_TEST=off \
     -DFMT_DOC=off \
     -DCMAKE_INSTALL_PREFIX="${cwd}/../" \
--- external/wxWidgets/src/build.sh
+++ external/wxWidgets/src/build.sh
@@ -7,12 +7,12 @@ mkdir -p build/ \
 cd build \
 && \
 ${CMAKE_CMD} "${CMAKE_GENERATOR}" ../wxWidgets-3.1.4 \
-    -DCMAKE_INSTALL_PREFIX="${cwd}/../" \
     -DwxBUILD_SHARED=OFF \
     -DwxBUILD_TESTS=OFF \
     -DwxBUILD_SAMPLES=ALL \
     -DwxBUILD_DEMOS=ON \
     -DwxBUILD_USE_STATIC_RUNTIME=ON \
     -DwxBUILD_MONOLITHIC=OFF \
+    -DCMAKE_INSTALL_PREFIX="${cwd}/../" \
 && \
 ${MSVC_BUILD} && ${MSVC_INSTALL}
