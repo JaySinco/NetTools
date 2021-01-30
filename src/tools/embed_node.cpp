17889135329780306af90eed2c2469dae7ba60da
jaysinco <jaysinco@163.com>
2021-01-30 18:12:08 +0800

    add create context func

--- src/tools/embed_node.cpp
+++ src/tools/embed_node.cpp
@@ -36,6 +36,13 @@ void log_js(const v8::FunctionCallbackInfo<v8::Value> &args)
     std::cout << *value << std::endl;
 }
 
+v8::Local<v8::Context> create_context(v8::Isolate *isolate)
+{
+    v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
+    global->Set(isolate, "log", v8::FunctionTemplate::New(isolate, log_js));
+    return node::NewContext(isolate, global);
+}
+
 int run_script(node::MultiIsolatePlatform *platform, const std::vector<std::string> &args,
                const std::vector<std::string> &exec_args, const std::string &source_path)
 {
@@ -45,51 +52,37 @@ int run_script(node::MultiIsolatePlatform *platform, const std::vector<std::stri
         std::cerr << "failed to init uv loop: {}"_format(uv_err_name(ret)) << std::endl;
         return 1;
     }
-
     std::shared_ptr<node::ArrayBufferAllocator> allocator = node::ArrayBufferAllocator::Create();
     v8::Isolate *isolate = NewIsolate(allocator.get(), &loop, platform);
     if (isolate == nullptr) {
         std::cerr << "failed to create v8 isolate" << std::endl;
         return 1;
     }
-
     {
         v8::Locker locker(isolate);
         v8::Isolate::Scope isolate_scope(isolate);
-
         std::unique_ptr<node::IsolateData, decltype(&node::FreeIsolateData)> isolate_data(
             node::CreateIsolateData(isolate, &loop, platform, allocator.get()),
             node::FreeIsolateData);
-
         v8::HandleScope handle_scope(isolate);
-
-        v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-        global->Set(v8::String::NewFromUtf8(isolate, "log"),
-                    v8::FunctionTemplate::New(isolate, log_js));
-
-        v8::Local<v8::Context> context = node::NewContext(isolate, global);
+        v8::Local<v8::Context> context = create_context(isolate);
         if (context.IsEmpty()) {
             std::cerr << "failed to create v8 context" << std::endl;
             return 1;
         }
-
         v8::Context::Scope context_scope(context);
-
         std::unique_ptr<node::Environment, decltype(&node::FreeEnvironment)> env(
             node::CreateEnvironment(isolate_data.get(), context, args, exec_args),
             node::FreeEnvironment);
-
         std::string code = R"(
             globalThis.require = require('module').createRequire(process.cwd() + '/');
             require('vm').runInThisContext(require('fs').readFileSync(`{0}`, encoding='utf8'), `{0}`);
         )"_format(source_path);
         v8::MaybeLocal<v8::Value> loadenv_ret = node::LoadEnvironment(env.get(), code.c_str());
-
         if (loadenv_ret.IsEmpty()) {
             std::cerr << "failed to load env, there has been a JS exception" << std::endl;
             return 1;
         }
-
         {
             v8::SealHandleScope seal(isolate);
             bool more;
@@ -104,18 +97,14 @@ int run_script(node::MultiIsolatePlatform *platform, const std::vector<std::stri
                 more = uv_loop_alive(&loop);
             } while (more == true);
         }
-
         exit_code = node::EmitExit(env.get());
-
         node::Stop(env.get());
     }
-
     bool platform_finished = false;
     platform->AddIsolateFinishedCallback(
         isolate, [](void *data) { *static_cast<bool *>(data) = true; }, &platform_finished);
     platform->UnregisterIsolate(isolate);
     isolate->Dispose();
-
     while (!platform_finished) {
         uv_run(&loop, UV_RUN_ONCE);
     }
@@ -152,7 +141,6 @@ int main(int argc, char **argv)
     std::unique_ptr<node::MultiIsolatePlatform> platform = node::MultiIsolatePlatform::Create(4);
     v8::V8::InitializePlatform(platform.get());
     v8::V8::Initialize();
-
     if (argc < 2 || !ends_with(argv[argc - 1], ".js")) {
         print_usage(argv[0]);
         return 1;
@@ -160,7 +148,6 @@ int main(int argc, char **argv)
     std::wstring native_path = s2ws(argv[argc - 1]);
     std::string generic_path = ws2s(std::filesystem::path(native_path).generic_wstring());
     int ret = run_script(platform.get(), args, exec_args, generic_path);
-
     v8::V8::Dispose();
     v8::V8::ShutdownPlatform();
     return ret;
