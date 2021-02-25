500db173b8bbdfa64b7e8b360bf0c606ef1e0a6a
jaysinco <jaysinco@163.com>
2021-02-25 22:07:58 +0800

    add uwebsocket test

--- src/submodule/module_test.cpp
+++ src/submodule/module_test.cpp
@@ -1,11 +1,34 @@
 #include "submodule/browser.h"
+#pragma warning(disable : 4138)
+#include "uwebSockets/src/App.h"
+
+void test_browser()
+{
+    browser br(L"test");
+    br.navigate(L"https://www.baidu.com");
+    br.wait_utill_closed();
+}
+
+void test_websocket(int port)
+{
+    uWS::App(uWS::SocketContextOptions{})
+        .get("/*", [](auto *res, auto * /*req*/) { res->end("Hello world!"); })
+        .listen("127.0.0.1", port,
+                [=](auto *listen_socket) {
+                    if (listen_socket) {
+                        LOG(INFO) << "listening on port " << port;
+                    }
+                })
+        .run();
+
+    LOG(ERROR) << "failed to listen on port " << port;
+}
 
 int main(int argc, char *argv[])
 {
     NT_TRY
     INIT_LOG(argc, argv);
-    browser br(L"test");
-    br.navigate(L"https://www.baidu.com");
-    br.wait_utill_closed();
+    // test_browser();
+    test_websocket(8000);
     NT_CATCH
 }
